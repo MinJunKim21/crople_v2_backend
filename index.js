8595b4adcc18d7a29758bccbf1f78e51f6f8591d
MinJunKim21 <jinju8012@gmail.com>
2022-12-04 08:32:00 +0900

    logout 오류중

--- index.js
+++ index.js
@@ -1,24 +1,57 @@
 const cookieSession = require('cookie-session');
+const dotenv = require('dotenv');
+const morgan = require('morgan');
 const express = require('express');
+const mongoose = require('mongoose');
 const cors = require('cors');
+const passportSetup = require('./passport');
 const passport = require('passport');
+const session = require('express-session');
+const MongoStore = require('connect-mongo');
+const authRoute = require('./routes/auth');
 const app = express();
+const connectDB = require('./config/db');
 
+// Load config
+dotenv.config({ path: './config/config.env' });
+
+// Passport config
+require('./config/passport')(passport);
+
+// Sessions
 app.use(
-  cookieSession({ name: 'session', keys: ['mj'], maxAge: 24 * 60 * 60 * 100 })
-);
-
-app.use(passport.initialize());
-app.use(passport.session());
-
-app.use(
-  cors({
-    origin: 'http://localhost:3000',
-    method: 'GET,POST,PUT,DELETE',
-    credetials: true,
+  session({
+    secret: 'keyboard cat',
+    resave: false,
+    saveUninitialized: false,
+    store: MongoStore.create({ mongoUrl: process.env.MONGO_URI }),
   })
 );
 
+// Passport middleware
+app.use(passport.initialize());
+app.use(passport.session());
+
+connectDB();
+
+// Logging
+if (process.env.NODE_ENV === 'development') {
+  app.use(morgan('dev'));
+}
+
+app.use(cors({ origin: 'http://localhost:3000', credentials: true }));
+app.set('trust proxy', 1);
+
+app.get('/getuser', (req, res) => {
+  res.send(req.user);
+});
+
+// Routes
+// app.use('/', require('./routes/index'));
+app.use('/auth', require('./routes/auth'));
+
+const PORT = process.env.PORT || 5001;
+
 app.listen('5001', () => {
-  console.log('server is running!');
+  console.log(`Server running in ${process.env.NODE_ENV} mode on port ${PORT}`);
 });
---
+++ passport.js
@@ -0,0 +1,27 @@
+const GoogleStrategy = require('passport-google-oauth20').Strategy;
+const passport = require('passport');
+
+const GOOGLE_CLIENT_ID =
+  '1076633736236-a1docblff9daunlt8esu4cvubhj0lp49.apps.googleusercontent.com';
+const GOOGLE_CLIENT_SECRET = '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';
+
+passport.use(
+  new GoogleStrategy(
+    {
+      clientID: GOOGLE_CLIENT_ID,
+      clientSecret: GOOGLE_CLIENT_SECRET,
+      callbackURL: '/auth/google/callback',
+    },
+    function (accessToken, refreshToken, profile, done) {
+      done(null, profile);
+    }
+  )
+);
+
+passport.serializeUser((user, done) => {
+  done(null, user);
+});
+
+passport.deserializeUser((user, done) => {
+  done(null, user);
+});
