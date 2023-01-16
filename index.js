c72dfa31b754807c059f3e9d50a3d6868582e1ca
MinJunKim21 <jinju8012@gmail.com>
2023-01-16 19:14:50 +0900

    [FIXING] trust proxy 삭제하면 안됨

--- index.js
+++ index.js
@@ -78,7 +78,7 @@ require('./config/passport')(passport);
 //   cookieSession({ name: "session", keys: ["rlaalswns"], maxAge: 24 * 60 * 60 * 100 })
 // );
 
-// app.set('trust proxy', 1);
+app.set('trust proxy', 1);
 
 // Sessions
 app.use(
