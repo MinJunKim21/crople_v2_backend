64612f35132093d6394c0c1ad3d8c4e88564afb5
MinJunKim21 <jinju8012@gmail.com>
2023-01-16 19:56:33 +0900

    [FIXING] cookie setting httponly 바꾼거는 영향없어서 session의 resave, saveuninitialized 옵션을 바꾸어봄-> 영향없어서 samesite none으로 바꿈

--- index.js
+++ index.js
@@ -88,7 +88,7 @@ app.use(
     saveUninitialized: false,
     store: MongoStore.create({ mongoUrl: process.env.MONGO_URI }),
     cookie: {
-      sameSite: 'none',
+      sameSite: none,
       secure: true,
       maxAge: 1000 * 60 * 60 * 24 * 7, // One Week
       httpOnly: false,
