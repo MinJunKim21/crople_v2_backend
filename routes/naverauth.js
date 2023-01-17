ae4b3fdba1c83e13f84a8d39a7c78fa2ce519857
MinJunKim21 <jinju8012@gmail.com>
2023-01-17 15:47:29 +0900

    [FIXING] res.redirect 클라url test

--- routes/naverauth.js
+++ routes/naverauth.js
@@ -18,7 +18,7 @@ router.get(
   '/naver/callback',
   passport.authenticate('naver', { failureRedirect: '/' }),
   (req, res) => {
-    res.redirect('CLIENT_URL');
+    res.redirect('https://croplev2.netlify.app');
   }
 );
 
