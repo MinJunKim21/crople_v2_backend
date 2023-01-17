f62fce6cec7f87f3f890e6b3b2b9e0c074111a2a
MinJunKim21 <jinju8012@gmail.com>
2023-01-17 15:51:35 +0900

    [FIXING] 크롬, 파폭 모두 정상 작동

--- routes/googleauth.js
+++ routes/googleauth.js
@@ -17,7 +17,7 @@ router.get(
   '/google/callback',
   passport.authenticate('google', { failureRedirect: '/' }),
   (req, res) => {
-    res.redirect(CLIENT_URL);
+    res.redirect('https://croplev2.netlify.app');
   }
 );
 
