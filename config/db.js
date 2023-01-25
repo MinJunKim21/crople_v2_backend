b38cf68ce809b92f178c9ae2e34ef0f7d0ba8eb8
MinJunKim21 <jinju8012@gmail.com>
2023-01-25 17:40:58 +0900

    [FIXING] unique nickName 특성이 적용안되어서 테스트

--- config/db.js
+++ config/db.js
@@ -3,8 +3,10 @@ const mongoose = require('mongoose');
 const connectDB = async () => {
   try {
     const conn = await mongoose.connect(process.env.MONGO_URI, {
-      useNewUrlParser: true,
       useUnifiedTopology: true,
+      useNewUrlParser: true,
+      useCreateIndex: true, //make this true
+      autoIndex: true,
     });
 
     console.log(`MongoDB Connected: ${conn.connection.host}`);
