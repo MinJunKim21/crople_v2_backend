c86d0969f1bafe4d2f44d728b8b164561bd9da33
MinJunKim21 <jinju8012@gmail.com>
2023-01-25 17:43:06 +0900

    [FIXING] sns로그인 안넘어가져서 복구

--- config/db.js
+++ config/db.js
@@ -3,10 +3,8 @@ const mongoose = require('mongoose');
 const connectDB = async () => {
   try {
     const conn = await mongoose.connect(process.env.MONGO_URI, {
-      useUnifiedTopology: true,
       useNewUrlParser: true,
-      useCreateIndex: true, //make this true
-      autoIndex: true,
+      useUnifiedTopology: true,
     });
 
     console.log(`MongoDB Connected: ${conn.connection.host}`);
