6302dd32782f3aa5252a71040306c6ae2019d04a
MinJunKim21 <jinju8012@gmail.com>
2023-01-25 13:11:50 +0900

    [FIXING] serializeuser 파트를 추가해봄, 크롬에서만 작동중

--- index.js
+++ index.js
@@ -129,6 +129,17 @@ app.use(
 app.use(passport.initialize());
 app.use(passport.session());
 
+assport.serializeUser((user, done) => {
+  return done(null, user._id);
+});
+
+passport.deserializeUser((id, done) => {
+  User.findById(id, (err, doc) => {
+    // Whatever we return goes to the client and binds to the req.user property
+    return done(null, doc);
+  });
+});
+
 connectDB();
 
 // Logging
